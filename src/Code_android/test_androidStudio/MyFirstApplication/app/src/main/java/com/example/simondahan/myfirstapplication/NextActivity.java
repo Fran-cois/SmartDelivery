5960a7c419c220ed838a04e2b8a9b08c7e868034
SimonDahan <simon.dahan@telecom-paristech.fr>
2017-01-19 16:03:01 +0100

    page1 de l'interface graphique

--- src/Code_android/test_androidStudio/MyFirstApplication/app/src/main/java/com/example/simondahan/myfirstapplication/NextActivity.java
+++ src/Code_android/test_androidStudio/MyFirstApplication/app/src/main/java/com/example/simondahan/myfirstapplication/NextActivity.java
@@ -1,8 +1,11 @@
 package com.example.simondahan.myfirstapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by SimonDahan on 04/12/2016.
@@ -13,6 +16,9 @@ public class NextActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent myIntend = getIntent();
+        String value = myIntend.getStringExtra("value");
         setContentView(R.layout.frame);
+
     }
 }
