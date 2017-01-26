4a935f6f1c834489058e3837cc228f0d964a8880
Benjamin Castellan <benjamincastellan@gmail.com>
2017-01-26 14:46:25 +0100

    add second activity

--- src/Code_android/test_androidStudio/appTP/app/src/main/java/com/example/benja/ressourcestest/MainActivity.java
+++ src/Code_android/test_androidStudio/appTP/app/src/main/java/com/example/benja/ressourcestest/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.benja.ressourcestest;
 
-import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Resources res = getResources();
-        TextView b1 = (TextView) findViewById(R.id.text1);
-        String text = res.getString(R.string.text, "Benjamin",20);
+        TextView b1 = (TextView) findViewById(R.id.textView1);
+        String text = getString(R.string.text, "Benjamin",20);
         b1.setText(text);
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
