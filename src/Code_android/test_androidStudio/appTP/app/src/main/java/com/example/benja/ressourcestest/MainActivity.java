aa033c8bd4ab439204511ad2705df227d4d7caa0
Benjamin Castellan <benjamincastellan@gmail.com>
2017-01-26 15:12:35 +0100

    modify second activity

--- src/Code_android/test_androidStudio/appTP/app/src/main/java/com/example/benja/ressourcestest/MainActivity.java
+++ src/Code_android/test_androidStudio/appTP/app/src/main/java/com/example/benja/ressourcestest/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -14,14 +15,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         TextView b1 = (TextView) findViewById(R.id.textView1);
         String text = getString(R.string.text, "Benjamin",20);
+
         b1.setText(text);
         Button button1 = (Button) findViewById(R.id.button1);
+
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        final String string1 = editText1.getText().toString();
+
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent1 = new Intent(MainActivity.this, SecondActivity.class);
+                intent1.putExtra("text_du_EditText",string1);
                 startActivity(intent1);
             }
         });
--- src/Code_android/test_androidStudio/appTP/app/src/main/java/com/example/benja/ressourcestest/SecondActivity.java
+++ src/Code_android/test_androidStudio/appTP/app/src/main/java/com/example/benja/ressourcestest/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.benja.ressourcestest;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends Activity {
 
@@ -10,5 +11,9 @@ public class SecondActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        textView2.setText(getIntent().getStringExtra("text_du_EditText"));
+
     }
 }
