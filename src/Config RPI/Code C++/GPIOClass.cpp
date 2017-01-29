a4333105cfdeb0d9ec9ee417290480a21c639237
ELYAAGOUBI Med Ayman <elyaagoubi.ayman@gmail.com>
2017-01-29 14:35:38 +0100

    Set_direction
    Ajout et implémentation de la méthode set_dir nécessaire.

--- src/Config RPI/Code C++/GPIOClass.cpp
+++ src/Config RPI/Code C++/GPIOClass.cpp
@@ -20,40 +20,56 @@ GPIOClass(string n)
 int export_gpio() // exporter la gpio créé afin d'éviter des problèmes de sudo (superutilisateur)
 {
 	string exp = "/sys/class/gpio/export";// ouvrir le répértoire d'export comme dans le terminal et l'affecter a exp
-	ofstream exportgpio(exp.c_str()); // ofstream : classe adaptée I/O qui prend exp et renvoie sa valeur quand on va la changer
-	exportgpio << this->gpion ; //on change exportgpu du répértoire en gpion le numéro du pin du gpio qu'on veut exporter, comme dans le terminal
-	exportgpio.close(); // on ferme le fichier export
+	ofstream expgpio(exp.c_str()); // ofstream : classe adaptée I/O qui prend exp et renvoie sa valeur quand on va la changer .c_str pour convertir le string c++ en string c par souci de compatibilité Linux
+	expgpio << this->gpion ; //on change exportgpu du répértoire en gpion le numéro du pin du gpio qu'on veut exporter, comme dans le terminal
+	expgpio.close(); // on ferme le fichier export
 	return 0;
 }
 
 int unexport_gpio()
 {
-	string imp = "/sys/class/gpio/unexport";// ouvrir le répértoire d'unexport comme dans le terminal et l'affecter a unexp
-	ofstream unexportgpio(unexp.c_str()); // ofstream : classe adaptée I/O
-	unexportgpio << this->gpion ; //on change unexportgpu du répértoire en gpion le numéro du pin du gpio qu'on veut exporter, comme dans le terminal
-	unexportgpio.close(); // on ferme le fichier unexport
+	string unexp = "/sys/class/gpio/unexport";// ouvrir le répértoire d'unexport comme dans le terminal et l'affecter a unexp
+	ofstream unexpgpio(unexp.c_str()); // ofstream : classe adaptée I/O .c_str pour convertir le string c++ en string c par souci de compatibilité Linux
+	unexpgpio << this->gpion ; //on change unexportgpu du répértoire en gpion le numéro du pin du gpio qu'on veut exporter, comme dans le terminal
+	unexpgpio.close(); // on ferme le fichier unexport
 	return 0;
 }
 
-int setval_gpio(string val)
+int set_val(string val)
 {
 
-	string setval_str = "/sys/class/gpio/gpio" + this->gpion + "/value"; //ouvrir le répértoire de la valeur du gpio, syntaxe proche du java,que du c.
-	ofstream setvalgpio(setval_str.c_str()); // ofstream : classe adaptée I/O
-	setvalgpio << val ; //affecter la valeur val au fichier valeur gpio
-	setvalgpio.close();// on ferme le fichier valeur
+	string setval = "/sys/class/gpio/gpio" + this->gpion + "/value"; //ouvrir le répértoire de la valeur du gpio, syntaxe proche du java,que du c.
+	ofstream setgpio(setval.c_str()); // ofstream : classe adaptée I/O .c_str pour convertir le string c++ en string c par souci de compatibilité Linux
+	setgpio << val ; //affecter la valeur val au fichier valeur gpio
+	setgpio.close();// on ferme le fichier valeur
 	return 0;
 }
 
-int getval_gpio(string& val){
+int get_val(string& val)
+{
 
-	string getval_str = "/sys/class/gpio/gpio" + this->gpion + "/value";
-	ifstream getvalgpio(getval_str.c_str());// ouvrir le répértoire de la valeur du gpio
-	getvalgpio >> val ;  // lecture de la valeur du gpio
-	if(val = "0")
+	string getval = "/sys/class/gpio/gpio" + this->gpion + "/value";
+	ifstream getgpio(getval.c_str());// ouvrir le répértoire de la valeur du gpio .c_str pour convertir le string c++ en string c par souci de compatibilité Linux
+	getgpio >> val ;  // lecture de la valeur du gpio
+	if(val = "0") //soit la valeur du gpio est nulle (0V) ou non nulle (3.3V), on effectue le test:
 		val = "0";
 	else
 		val = "1";
-	getvalgpio.close(); //on ferme le fichier valeur
+	getgpio.close(); //on ferme le fichier valeur
     return 0;
+}
+
+string get_gpion()
+{
+	return this->gpion;
+}
+
+int set_dir(string dir)
+{
+
+	string setdir ="/sys/class/gpio/gpio" + this->gpionum + "/direction";
+	ofstream setdirgpio(setdir.c_str()); // ouvrir le répértoire de la direction du gpio .c_str pour convertir le string c++ en string c par souci de compatibilité Linux
+	setdirgpio << dir; // affecter la direction dir au fichier direction gpio
+	setdirgpio.close(); // on ferme le fichier direction
+	return 0;
 }
--- src/Config RPI/Code C++/GPIOclass.h
+++ src/Config RPI/Code C++/GPIOclass.h
@@ -1,10 +1,9 @@
 #include <string>
-/* GPIO Class
- * n : pin number must be passed to the class's constructor
- */
 using namespace std; // Pour avoir GPIOClass:: avant les méthodes (je ne comprends pas trop l'utilité mais c'est utilisé dans tous les progs c++ donc bon)
 
-
+/* GPIO Class
+ * n : le numéro du pin, pris en argument par le constructeur de la classe GPIO
+ */
 class GPIOClass // déclaration de la classe GPIO
 {
 public:
@@ -14,4 +13,5 @@ public:
     int set_val(string val); // mettre la valeur du GPIO (ne marchera que pour les outputs => nécessité d'ajouter des méthodes pour savoir l'orientation du GPIO)
     int get_val(string& val); // lire la valeur du GPIO
     string get_num(); // Lire le numéro n du GPIO
+    int set_dir(string dir); // Configurer la direction du GPIO
 };
