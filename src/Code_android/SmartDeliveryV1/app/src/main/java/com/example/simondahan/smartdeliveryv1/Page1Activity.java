c204d80ef422f360fefec4f8e32138a8e669a212
SimonDahan <simon.dahan@telecom-paristech.fr>
2017-01-30 10:07:56 +0100

    gitignore

--- src/Code_android/SmartDeliveryV1/app/src/main/java/com/example/simondahan/smartdeliveryv1/Page1Activity.java
+++ src/Code_android/SmartDeliveryV1/app/src/main/java/com/example/simondahan/smartdeliveryv1/Page1Activity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Page1Activity extends AppCompatActivity{
+public class Page1Activity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
