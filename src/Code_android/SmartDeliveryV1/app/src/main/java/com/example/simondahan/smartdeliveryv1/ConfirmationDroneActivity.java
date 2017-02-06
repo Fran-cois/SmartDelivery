1ad1ad5ad15073e70d104cfa5bda5d5db074dd08
Benjamin Castellan <benjamincastellan@gmail.com>
2017-02-06 09:30:46 +0100

    changement des heritages appCompatibility

--- src/Code_android/SmartDeliveryV1/app/src/main/java/com/example/simondahan/smartdeliveryv1/ConfirmationDroneActivity.java
+++ src/Code_android/SmartDeliveryV1/app/src/main/java/com/example/simondahan/smartdeliveryv1/ConfirmationDroneActivity.java
@@ -1,9 +1,9 @@
 package com.example.simondahan.smartdeliveryv1;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
@@ -11,7 +11,7 @@ import android.widget.TextView;
 
 import java.sql.ResultSet;
 
-public class ConfirmationDroneActivity extends Activity {
+public class ConfirmationDroneActivity extends AppCompatActivity {
 
 
 
--- src/Code_android/SmartDeliveryV1/app/src/main/java/com/example/simondahan/smartdeliveryv1/DroneArriveActivity.java
+++ src/Code_android/SmartDeliveryV1/app/src/main/java/com/example/simondahan/smartdeliveryv1/DroneArriveActivity.java
@@ -3,10 +3,11 @@ package com.example.simondahan.smartdeliveryv1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class DroneArriveActivity extends Activity {
+public class DroneArriveActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
