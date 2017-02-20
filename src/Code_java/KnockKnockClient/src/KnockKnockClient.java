3d28038e3770763d0dd5cb829d8808bc0bf90b21
clochard <kei-saburo.clochard@telecom-paristech.fr>
2017-02-20 09:54:39 +0100

    Debut du code C++

--- src/Code_java/KnockKnockClient/src/KnockKnockClient.java
+++ src/Code_java/KnockKnockClient/src/KnockKnockClient.java
@@ -1,3 +1,5 @@
+package Code_java.KnockKnockClient.src;
+
 import java.io.*;
 import java.net.*;
 import java.util.logging.*;
--- src/Code_java/KnockKnockClient/src/ProtocolClient.java
+++ src/Code_java/KnockKnockClient/src/ProtocolClient.java
@@ -1,3 +1,5 @@
+package Code_java.KnockKnockClient.src;
+
 
 
 public class ProtocolClient {
