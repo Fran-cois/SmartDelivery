ae3ea0dbbce99fbf488336590b1c06cd6220535f
SimonDahan <simon.dahan@telecom-paristech.fr>
2017-03-02 17:45:24 +0100

    test

--- src/Code_python/Estimation_mouvement/OpenCv/test.py
+++ src/Code_python/Estimation_mouvement/OpenCv/test.py
@@ -6,7 +6,8 @@ cap = cv2.VideoCapture("test.MOV")
 while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    small = cv2.resize(frame, (0,0), fx=0.3, fy=0.3) 
+    gray = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
 
     # Our operations on the frame come here
     print(1)
