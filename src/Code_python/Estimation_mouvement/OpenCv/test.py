b7eced4ee4cd6acff404905b4dfce2360b7e4eec
SimonDahan <simon.dahan@telecom-paristech.fr>
2017-03-02 17:48:59 +0100

    test

--- src/Code_python/Estimation_mouvement/OpenCv/test.py
+++ src/Code_python/Estimation_mouvement/OpenCv/test.py
@@ -11,6 +11,28 @@ while(cap.isOpened()):
     edges = cv2.Canny(gray,0,150,apertureSize = 3)
 
     lines = cv2.HoughLines(edges,1,np.pi/180,200)
+    if (lines is None):
+            continue
+    else:
+        tab=lines[0]
+        b=np.shape(tab)[0]
+        s=0
+        for i in range(b):
+            s=s+abs(tab[i][0])
+        m=s/b
+
+        tab1=[]
+        tab2=[]
+
+        for i in range(b):
+            if(abs(tab[i][0])<m):
+                tab1.append(tab[i])
+            else:
+                tab2.append(tab[i])
+
+        tab1=sorted(tab1, key=lambda colonnes: colonnes[1])
+        tab2=sorted(tab2, key=lambda colonnes: colonnes[1])
+
 
     # Our operations on the frame come here
     print(1)
