23b0d7cc4537dbbb3c9102735c9ff6e813670c59
fran-cois <francois.amat@telecom-paristech.fr>
2017-03-17 09:50:38 +0100

    v0 of main.py -> communication between threads

--- src/main.py
+++ src/main.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python
 import threading
 import time
+import Queue
+# declaration des variables 
+q = Queue.Queue()
 
-def worker():
+# fin declaration des variables
+def sendOrderToDrone():
     print threading.currentThread().getName(), 'Starting'
-    time.sleep(2)
-    print threading.currentThread().getName(), 'Exiting'
+    compteur = 0 
+    # traitement des variables dans la queue 
+    while True:
+	    if not q.empty():
+	    	print q.get()
+	    	compteur +=1 
+	    time.sleep(2)
+	    
+	    
+	     # permet de quiter le programme (pour les tests) 
+	    print threading.currentThread().getName(), 'Exiting')
+	    if compteur >0 :
+	    	exit()
 
-def my_service():
+def SendOrderToThread(symbole):
     print threading.currentThread().getName(), 'Starting'
+    q.put(symbole)
+    print "sending 2 to the queue "
     time.sleep(3)
     print threading.currentThread().getName(), 'Exiting'
 
-t = threading.Thread(name='my_service', target=my_service)
-w = threading.Thread(name='worker', target=worker)
-w2 = threading.Thread(target=worker) # use default name
+# PERMET d'envoyer le symbole dans arg à l'autre thread 
+w1 = threading.Thread(name='SendOrderToThread', target=SendOrderToThread,args="+")
+# defini le thread qui lance le drone 
+w2 = threading.Thread(name='sendOrderToDrone', target=sendOrderToDrone)
 
-w.start()
+
+
+w1.start()
 w2.start()
-t.start()
