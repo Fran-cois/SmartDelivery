f98d60d96f06ebc4dd2ec17241a5ad70667a2337
Benjamin Castellan <benjamincastellan@gmail.com>
2017-04-11 15:33:03 +0200

    lecture du QRcode a partir d'un fichier texte

--- src/Code_java/KnockKnockServer/src/Drone.java
+++ src/Code_java/KnockKnockServer/src/Drone.java
@@ -1,16 +1,36 @@
+import java.io.*;
 
 public class Drone {
+
 	public static String getRoom(){
+
 		String salle;
+
+		/*  Ce qui suit etait le premier test
+
 		double proba=Math.random();
 		if (proba<0.75){
 			salle="4";
 		}
 		else{
 			salle="3";
-		}
+		}*/
+
+		//2eme test avec lecture dans un fichier
+        String fichier = "valeur_qrcode.txt";
+
+        //lecture fichier
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fichier));
+            salle = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            salle = "0";
+        }
+
 		return salle;
 	}
+
 	public static boolean available(){
 		return true;
 	}
---
+++ src/Code_java/KnockKnockServer/src/test_valeur_QRcode.py
@@ -0,0 +1,10 @@
+from random import randint  #pour test sans analyse image
+import time
+
+for i in range(20):
+    qrcode = str(randint(1,5))   #pour test sans analyse image
+    mon_fichier = open('valeur_qrcode.txt','w')   #on ecrase ou on creer le fichier
+    mon_fichier.write(qrcode)  #on ecrit le numero de la salle lu a partir du QRcode
+    mon_fichier.close()
+    print (qrcode)
+    time.sleep(4)
