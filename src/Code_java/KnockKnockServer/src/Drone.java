b3ef61ffc92fef6222ded9f461afc9b8bfd4d8d7
Benjamin Castellan <benjamincastellan@gmail.com>
2017-04-14 17:58:32 +0200

    correction lecture fichier

--- src/Code_java/KnockKnockServer/src/Drone.java
+++ src/Code_java/KnockKnockServer/src/Drone.java
@@ -5,6 +5,7 @@ public class Drone {
 	public static String getRoom(){
 
 		String salle;
+		String salleLue;
 
 		/*  Ce qui suit etait le premier test
 
@@ -22,17 +23,36 @@ public class Drone {
         //lecture fichier
         try {
             BufferedReader br = new BufferedReader(new FileReader(fichier));
-            salle = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-            salle = "0";
+            salleLue = br.readLine();
+            return (valeurSalle(salleLue));
+            } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+            return ("0");  //on n'arrive pas a lire le fichier
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            return ("0");
         }
-
-		return salle;
 	}
 
 	public static boolean available(){
 		return true;
 	}
 
+	public static String valeurSalle(String salleLue) {
+	    if (salleLue.contains("1")) {
+	        return "1";
+        }
+        if (salleLue.contains("2")) {
+            return "2";
+        }
+        if (salleLue.contains("3")) {
+            return "3";
+        }
+        if (salleLue.contains("4")) {
+            return "4";
+        }
+        if (salleLue.contains("5")) {
+            return "5";
+        }
+    }
 }
