35af1b6d85bff0bea03c49792c195a7a574116e1
Benjamin Castellan <benjamincastellan@gmail.com>
2017-04-22 16:46:30 +0200

    modification KnockKnockClient qui marche avec QRcode

--- src/Code_android/SmartDeliveryV1/app/src/main/java/com/example/simondahan/smartdeliveryv1/KnockKnockClient.java
+++ src/Code_android/SmartDeliveryV1/app/src/main/java/com/example/simondahan/smartdeliveryv1/KnockKnockClient.java
@@ -25,6 +25,7 @@ public class KnockKnockClient extends AsyncTask<Object, Object, Boolean> {
 
         private String[] args;
         private Activity mActivity;
+        private boolean arrived;
 
 
         public KnockKnockClient (String[] args, Activity mActivity) {
@@ -72,7 +73,7 @@ public class KnockKnockClient extends AsyncTask<Object, Object, Boolean> {
 
                     Log.i("Client", "doInBackgroundl73");
                     String fromServer;
-
+            arrived = false;
             try {
                 while ((fromServer = in.readLine()) != null) {
                     Log.i("Client","Debut du while");
@@ -128,12 +129,80 @@ public class KnockKnockClient extends AsyncTask<Object, Object, Boolean> {
                     }
                     if (fromServer.equals("Bye. I arrived.")){
                         Log.i("Client","Server: " + fromServer);
+                        arrived = true;
                         break;}
                 }
             } catch (IOException e) {
                 Log.w("Client","Impossible de lire les lignes");
             } catch (InterruptedException e) {
                 Log.w("Client","Thread sleep ne s'execute pas.");
+            }
+
+            while(!arrived) {
+                try {
+                    fromServer = in.readLine();
+                    Log.i("Client","Debut du while");
+                    if (fromServer.equals("I'm listening.")){
+                        out.println("I would like to make a delivery. Is the drone available?");
+                        Log.i("Client","Server: " + fromServer);
+                        Log.i("Client","Client: I would like to make a delivery. Is the drone available?");
+                    }
+                    if(fromServer.equals("try again")){
+                        Thread.sleep(period);   // Le client patiente avant de demander une autre connection.
+                        out.println("I would like to make a delivery. Is the drone available?");
+                        Log.w("Client","Server: " + fromServer);
+                        Log.i("Client","Client: I would like to make a delivery. Is the drone available?");
+                    }
+                    if (fromServer.equals("yes")){
+                        ProtocolClient.available=true;
+                        out.println("go to the room "+ getFinalRoom()); //Application android
+                        Log.i("Client","Server: " + fromServer);
+                        Log.i("Client","Client: go to the room "+getFinalRoom());
+                    }
+                    if(fromServer.equals("no")){
+                        Thread.sleep(period);   // Le client patiente avant de demander une autre livraison
+                        out.println("I would like to make a delivery. Is the drone available?");
+                        Log.i("Client","Server: " + fromServer);
+                        Log.i("Client","Client: I would like to make a delivery. Is the drone available?");
+                    }
+                    if(fromServer.equals("I didn't get the room. Where do you want me to go?")){
+                        Thread.sleep(period);   // Le client patiente avant de redemander la salle.
+                        out.println("go to the room "+ getFinalRoom()); //Application android
+                        Log.w("Client","Server: " + fromServer);
+                        Log.i("Client","Client: go to the room "+getFinalRoom());
+                    }
+                    Log.i("Client","fromServer egale : "+fromServer);
+                    if (fromServer.equals("I'm going.")||fromServer.contains("I'm in the room ") ||fromServer.contains("I didn't get your question. Can you repeat?")){
+                        try {
+                            Thread.sleep(period);   // Le client patiente avant de demander où le drone est.
+                            out.println("where are you?");
+                            if(fromServer.contains("I'm in the room ")){
+                                ProtocolClient.localisation=fromServer.substring(16);
+                                Log.i("Client","Server: " + fromServer);
+                                Log.i("Client","localisation "+ProtocolClient.localisation);
+                            }
+                            if (fromServer.contains("I didn't get your question. Can you repeat?")){
+                                Log.w("Client","Server: " + fromServer);
+                                Log.i("Client","Client: where are you?");
+                            }
+                            else{
+                                Log.i("Client","Server: " + fromServer);
+                                Log.i("Client","Client: where are you?");}
+                        } catch (InterruptedException e) {
+                            Log.w("Client","erreur try line 101");
+                        }
+                    }
+                    if (fromServer.equals("Bye. I arrived.")){
+                        Log.i("Client","Server: " + fromServer);
+                        arrived = true;
+                        break;}
+
+
+                } catch (IOException e) {
+                    Log.w("Client","Impossible de lire les lignes");
+                } catch (InterruptedException e) {
+                    Log.w("Client","Thread sleep ne s'execute pas.");
+                }
             }
             try {
                 kkSocket.close();
